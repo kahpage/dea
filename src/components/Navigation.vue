17f96c44c952a66f621e241f493607a5a86d9334
kahpage <216363504+kahpage@users.noreply.github.com>
2025-06-17 22:16:51 +0800

    Navigation according to base

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,9 +6,9 @@
     <!-- Navigation Links -->
   <nav class="navigation">
     <div class="navigation-buttons">
-      <a href="/" class="navigation-button">Home</a>
-      <a href="/events/" class="navigation-button">Event list</a>
-      <a href="/about/" class="navigation-button">About</a>
+      <a href="/dea/" class="navigation-button">Home</a>
+      <a href="/dea/events/" class="navigation-button">Event list</a>
+      <a href="/dea/about/" class="navigation-button">About</a>
     </div>
     <div class="navigation-title">dea</div>
   </nav>
