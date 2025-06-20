a6e9d5dd1b6ddc972e9dbf7fbc9d406d694c0979
kahpage <216363504+kahpage@users.noreply.github.com>
2025-06-21 01:17:42 +0800

    Increased timeout time

--- src/axios/axios_config.js
+++ src/axios/axios_config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 //   baseURL: 'https://api.example.com', // Set your base URL
-  timeout: 1000,
+  timeout: 5000,
 //   headers: {'X-Custom-Header': 'foobar'}
 });
 
