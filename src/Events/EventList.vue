6bac42bd6a6649be38b4c8ec6e7aaeeec91c7f60
kahpage <216363504+kahpage@users.noreply.github.com>
2025-06-20 19:49:04 +0800

    Simple tables with fetch

--- src/Events/EventList.vue
+++ src/Events/EventList.vue
@@ -2,33 +2,30 @@
      Event List tab
 ... ============================================================ -->
 
-<style>
-    @import '@/assets/common.css';
-</style>
-
 <script setup>
   import { ref } from "vue";
   import axiosInstance from '@/axios/axios_config.js';
+  import EventGroup from "./components/EventGroup.vue";
   const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
   const loading_done = ref(false);
   const loaded_counter = ref(0);
   const loading_current = ref("");
-  // const jsonData = ref([]);
+  const json_event_groups = ref([]);
 
   const public_path = import.meta.env.MODE == 'production' ? `/dea/` : `../`
   
   function fetch_db_index() {
     loading_done.value = false; 
-    loaded_counter.value = 0; 
     loading_current.value = ''; // Reset
     axiosInstance.get( `${public_path}databases/database_file_index.json`)
       .then(response => {
-        console.log(response.data);
+        // console.log(response.data);
         for (const db_name in response.data) {
           loading_current.value = db_name;
-          if (fetch_db(response.data[db_name])) {loaded_counter.value++;} // increase IF loaded          
+          if (fetch_db(response.data[db_name])) {} // increase IF loaded          
         }
+        // console.log(json_event_groups);
         loading_done.value = true;
       })
       .catch(error => {
@@ -40,8 +37,10 @@
     // Return the promise chain
     return axiosInstance.get(`${public_path}databases/${db_name}/${db_name}.json`)
       .then(response => {
-        console.log(response.data);
-        return true; // TODO: not good enough check (check if valid json too ?)
+        // console.log(response.data);
+        if (!response.data.hasOwnProperty("aliases")) {return false;} // Check if invalid !
+        json_event_groups.value.push(response.data)
+        return true; 
       })
       .catch(error => {
         console.error('Error fetching data:', error); // Log any errors that occur during the fetch
@@ -61,15 +60,17 @@
 
     <a v-if="!loading_done">
       <div class="header">Event list not yet loaded.</div>
-      <div class="header">{{ loaded_counter }} loaded databases.</div>
+      <div class="header">{{ json_event_groups.length }} loaded databases.</div>
       <div class="header" v-if="loading_current!==''">Fetching {{ loading_current }} ...</div>
     </a>
     <a v-else>
-      <div class="header">Event list loaded ! ( {{ loaded_counter }} )</div>
+      <div class="header">Event list loaded ! ( {{ json_event_groups.length }} )</div>
     </a>
     <hr>
 
-    
+    <EventGroup v-for="(eg, i) in json_event_groups" :key="i" :event_group_db="eg" />
 </template>
 
-
+<style>
+    @import '@/assets/common.css';
+</style>
