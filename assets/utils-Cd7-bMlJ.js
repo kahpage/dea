f1ba425767d1a0641d4b44eef55f67b4316bfecf
kahpage <216363504+kahpage@users.noreply.github.com>
2025-07-16 00:19:52 +0800

    gh-pages

---
+++ assets/utils-Cd7-bMlJ.js
@@ -0,0 +1,1 @@
+function n(a){const t=/(https?:\/\/[^\s,]+)/g;return a.replace(t,function(e){return'<a class="embedded-link" href="'+e+'" target="_blank">'+e+"</a>"})}const s="/dea/";export{n as m,s as p};
