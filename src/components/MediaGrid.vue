3586067d1a374f1fa2990608e9978b71e3803c5d
kahpage <216363504+kahpage@users.noreply.github.com>
2025-07-25 22:04:59 +0800

    Added support for external media in MediaGrid.

--- src/components/MediaGrid.vue
+++ src/components/MediaGrid.vue
@@ -3,25 +3,58 @@
 ... ============================================================ -->
 
 <template>
-  <div class="media-div"
-    v-if="!props.media_list || Array.isArray(props.media_list) & (props.media_list.length == 0)"
+  <div
+    class="media-div"
+    v-if="
+      !props.media_list ||
+      Array.isArray(props.media_list) & (props.media_list.length == 0)
+    "
   >
     (None)
   </div>
   <div v-else class="media-div">
     <div class="media">
-      <div class="medium" v-for="(media, index) in props.media_list" :key="index">
+      <div
+        class="medium"
+        v-for="(media, index) in props.media_list"
+        :key="index"
+      >
+        <!-- Link (externally hosted media) -->
+        <div v-if="media.path.startsWith('http')">
+          <a
+            class="format-link"
+            :href="props.media_folder_path + '/' + media.path"
+            target="_blank"
+            title="External link to media"
+          >
+            <span class="format-link-arrow">⭷</span>
+            {{
+              media.path
+                .replace(/^\/+|\/+$/g, "")
+                .split("/")
+                .at(-1)
+            }}
+          </a>
+        </div>
 
         <!-- Image -->
-        <img class="format-image" v-if="isImage(media)" 
-          :src="props.media_folder_path + '/' + media.path"
-          :alt="'Image ' + media.path"
-          style="max-width: 100%; height: auto"
-        />
+        <div v-else-if="isImage(media)">
+          <img
+            class="format-image"
+            :src="props.media_folder_path + '/' + media.path"
+            :alt="'Image ' + media.path"
+            style="max-width: 100%; height: auto"
+          />
+        </div>
 
         <!-- Other format -->
         <div v-else>
-          <a class="format-other" :href="props.media_folder_path + '/' + media.path" target="_blank" title="Other file type">
+          <a
+            class="format-other"
+            :href="props.media_folder_path + '/' + media.path"
+            target="_blank"
+            title="Other file type"
+          >
             {{ media.path }}
           </a>
         </div>
@@ -35,13 +68,15 @@
             <span v-html="makeLinksClickable(source_.source)"></span>
           </div>
         </div>
+        <hr v-if="index < props.media_list.length - 1" />
+        <!-- Exclude last -->
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import {makeLinksClickable} from "@/assets/utils.js";
+import { makeLinksClickable } from "@/assets/utils.js";
 
 const props = defineProps({
   media_list: Array, // List of media objects
@@ -49,9 +84,12 @@ const props = defineProps({
 });
 
 function isImage(media) {
-  return media && media.path && /\.(jpg|jpeg|png|gif|webp|svg)$/i.test(media.path.toLowerCase());
+  return (
+    media &&
+    media.path &&
+    /\.(jpg|jpeg|png|gif|webp|svg)$/i.test(media.path.toLowerCase())
+  );
 }
-
 </script>
 
 <style scoped>
@@ -60,26 +98,54 @@ function isImage(media) {
 /* media */
 .media-div {
   padding: 1em;
-  box-sizing: border-box; 
+  box-sizing: border-box;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(20em, 1fr));
   gap: 1em;
   justify-content: center;
   overflow-y: scroll;
   background-color: var(--greyish-dark);
-  max-height: 40vh;
+  max-height: 100vh;
   border: 1px solid var(--greyish-soft);
+}
+
+.media-div hr {
+  border: none;
+  border-top: 1px solid var(--greyish-soft);
+  margin: 0.5em 0;
 }
 
 .medium {
   margin-top: 0.5em;
 }
 
-.format-image {}
-a.format-other{
+a.format-link {
   color: var(--green-soft);
   text-decoration: none;
   font-weight: bold;
 }
+.format-link-arrow {
+  color: var(--green-soft);
+}
 
+a.format-link:hover {
+  text-decoration: underline;
+}
+
+a.format-image {
+}
+.format-image {
+}
+
+.format-other {
+  color: var(--scarlet-soft);
+}
+a.format-other {
+  color: var(--scarlet-soft);
+  text-decoration: none;
+  font-weight: bold;
+}
+a.format-other:hover {
+  text-decoration: underline;
+}
 </style>
